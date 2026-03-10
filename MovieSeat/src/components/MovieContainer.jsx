880f512e408199fb56b2a172deb764ae83b16f6c
Chilungu Johansson <chilungu.johansson@yh.nackademin.se>
2026-03-10 17:53:39 +0100

    changed so that movies.json is no longer imported and instead is fetched. Also changed the location of movies to public folder.

--- MovieSeat/src/components/MovieContainer.jsx
+++ MovieSeat/src/components/MovieContainer.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
-import moviesData from "/Json/movies.json";
 
 function MovieContainer({ onMovieChange, onPriceChange }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("/movies.json")
+            .then(res => res.json())
+            .then(data => setMovies(data));
+    }, []);
+
     const handleMovieChange = (e) => {
         const title = e.target.value;
-
-        if (title === "") {
-            onMovieChange(null);
-            onPriceChange(0);
-            return;
-        }
-
-        const movie = moviesData.find(m => m.Title === title) || null;
+        const movie = movies.find(m => m.Title === title);
 
         onMovieChange(movie);
         onPriceChange(movie ? movie.Price : 0);
@@ -21,8 +21,8 @@ function MovieContainer({ onMovieChange, onPriceChange }) {
         <div className="movie-container">
             <label htmlFor="movie">Pick a movie:</label>
             <select name="movie" id="movie" onChange={handleMovieChange}>
-                <option value="0">Select a movie</option>
-                {moviesData.map(movie => (
+                <option value="">Select a movie</option>
+                {movies.map(movie => (
                     <option key={movie.Title} value={movie.Title}
                     > {movie.Title} ({movie.Price} kr)
                     </option>
